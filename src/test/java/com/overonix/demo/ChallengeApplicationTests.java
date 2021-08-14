2ecc93812f7df5a5da10e8a92c42a192f2e60da2
Roman <matviichuk.office@gmail.com>
2021-08-14 13:54:36 +0300

    test commit

---
+++ src/test/java/com/overonix/demo/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overonix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
